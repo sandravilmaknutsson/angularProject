b8ba95734637af1caf92286b0bd3ada6fed9fd81
Sandra Knutsson <sandra.knutsson@bahnhof.se>
2024-08-07 13:55:41 +0200

    created filtering of data in table by adding an input field

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -3,11 +3,14 @@ import { Course } from '../model/course';
 import { CourseService } from '../services/course.service';
 import { CommonModule } from '@angular/common';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatInputModule, MatFormFieldModule, MatSortModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.scss'
 })
@@ -23,5 +26,10 @@ export class CourseListComponent {
       this.courseList = data;
       this.dataSource = new MatTableDataSource<Course>(this.courseList)
     })
+  }
+
+  filter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
