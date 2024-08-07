eb4e962ecae01b34ab766d16cc0f403b0f2a0f65
Sandra Knutsson <sandra.knutsson@bahnhof.se>
2024-08-07 13:28:05 +0200

    created a service-folder and added a service for handling course-data. Also created a model-folder with a course-interface that handles structure of course-data. Furthermore I built a simple table using angular material and showcased course-data in table-element

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CourseService } from '../services/course.service';
+import { CommonModule } from '@angular/common';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.scss'
 })
 export class CourseListComponent {
+  displayedColumns: string[] = ["name", "points", "subject"];
+  courseList: Course[] = [];
+  // dataSource = new MatTableDataSource(this.courseList);
 
+  constructor(private courseService: CourseService) { }
+
+  ngOnInit() {
+    this.courseService.getCourses().subscribe(data => {
+      this.courseList = data;
+    })
+  }
 }
---
+++ src/app/services/course.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  private url: string = "assets/courses.json";
+
+  constructor(private http: HttpClient) { }
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
+  }
+}
