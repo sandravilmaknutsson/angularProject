7ea014a83c853a79481fbff0d0d244a0772eece1
Sandra Knutsson <sandra.knutsson@bahnhof.se>
2024-08-13 10:41:40 +0200

    added more design to overall page, fixed columns and a proper 404 page. Also hid header and footer on 404 page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angularProject';
+  showHeader: boolean = true;
+  showFooter: boolean = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        if (val.url === "/" || val.url === "/courseList" || val.url === "/individualSchedule") {
+          this.showHeader = true;
+          this.showFooter = true;
+        }
+        else {
+          this.showHeader = false;
+          this.showFooter = false;
+        }
+      }
+    })
+  }
 }
