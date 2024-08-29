a333962f2e3d304ebe673d784e9112b612a444ff
Sandra Knutsson <sandra.knutsson@bahnhof.se>
2024-08-29 15:51:43 +0200

    working on routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
-
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), HashLocationStrategy]
 
 };
